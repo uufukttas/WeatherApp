cdd6bcab88992e8bf839d934fe897917ac3cffb2
uufukttas <uufukttas@hotmail.com>
2021-12-24 00:42:56 +0300

    Changed background image according to temperature.

--- src/Components/FindCity.js
+++ src/Components/FindCity.js
@@ -7,23 +7,48 @@ function FindCity() {
 
     const onSubmit = (event) => {
         event.preventDefault();
-        getData(form.city)
+        getData(form.city);
+        setBackground();
     };
 
     const onChangeInput = (event) => {
         setForm({...form, [event.target.name]: event.target.value })
-    }
+    };
 
     const getData = async (city) => {
-        axios(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`).then(res => convertToCelcius(res.data.main.temp))
-    }
+        axios(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`).then(res => convertToCelcius(res.data.main.temp));
+    };
 
     const convertToCelcius = temp => {
         setForm({...form, 'value': Math.floor(Number(temp) - 273.5) })
     };
 
+    const setBackground = () => {
+        document.getElementById('root').style.backgroundPosition = 'center center';
+        document.getElementById('form-container').style.backgroundColor = 'rgba(255,255,255,0.7)';
+        document.getElementById('root').style.backgroundImage = `url(${findImage(form.value)})`;
+    };
+
+    const findImage = temperature => {
+        let image;
+        
+            if (temperature <= 0) {
+                image = '/snow.jpg';
+            } else if (temperature <= 10 && temperature > 0) {
+                image = '/rainy.jpg';
+            } else if (temperature <= 20 && temperature > 10) {
+                image = '/spring.jpg';
+            } else if (temperature > 20) {
+                image = '/sunny.jpg';
+            } else {
+                image = '';
+        }
+
+        return image;
+    };
+
     return (
-        <div className='input p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center justify-center aling-center flex-col'>
+        <div id='form-container' className='input p-6 max-w-sm ml-auto bg-white rounded-xl shadow-lg flex items-center justify-center aling-center flex-col'>
             <form onSubmit={onSubmit} className='flex justify-center aling-center'>
                 <label htmlFor='city' className='px-2'>Enter a city</label>
                 <input name="city" id="city" type="text" value={form.city} onChange={onChangeInput} className='rounded-md border-2 border-black'/>
